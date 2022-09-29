63bf250f36dc2981dbb48d018882cb3933711f4d
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-09-29 21:09:07 +0100

    add title and resume with gradiant color and darkmode icon

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -10,10 +11,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        
-       
+      <main className="px-10">
+        <section className="h-screen">
+          <nav className="flex justify-between py-10 mb-12">
+            <h1 className="text-xl">mythosedevelop</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
+              </li>
+              <li>
+                <a className="bg-gradient-to-r from-cyan-600 to-teal-600 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a>
+              </li>
+            </ul>
+          </nav>
+        </section>
       </main>
     </div>
-  )
+  );
 }
