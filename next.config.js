fa94e75d5c465aaeb570679991aa523702923f50
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-09-30 23:16:43 +0100

    configure build folder, image loader and base path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPlugins([{
   reactStrictMode: true,
   swcMinify: true,
-}
+  distDir: "build",
+  images: {
+    loader: 'custom'
+  },
+  basePath: '/out'
+    
+}])
 
 module.exports = nextConfig
