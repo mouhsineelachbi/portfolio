ff7007e4f136866d2066fa27e13fbf212f476bd8
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-09-30 23:15:10 +0100

    create image loader

---
+++ pages/image.js
@@ -0,0 +1,12 @@
+// components/Image.js
+
+import NextImage from "next/image";
+
+// opt-out of image optimization, no-op
+const customLoader = ({ src }) => {
+  return src;
+};
+
+export default function Image(props) {
+  return <NextImage {...props} loader={customLoader} />;
+}
